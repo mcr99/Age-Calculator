d28f7a97486aa956fba05d30335bc71c1441c5fd
marvin <mcanastuj1999@gmail.com>
2026-02-21 23:40:36 -0600

    adding updates on age calculation js file

---
+++ scripts/scripts.js
@@ -0,0 +1,11 @@
+let inputDay = document.querySelector("#day")
+let inputMonth = document.querySelector("#month")
+let inputYear = document.querySelector("#year")
+
+let actualDate = new Date()
+let actualDay = actualDate.getDate()
+let actualMonth = (actualDate.getMonth()+1)
+let actualYear = actualDate.getFullYear()
+
+// calcular por dias
+
